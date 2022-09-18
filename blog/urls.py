0d81c6ea155fb92af17d4aa32f4d417737e967eb
dennis mbithi <denmbithi@gmail.com>
2022-09-18 11:01:07 +0300

    Add class based list view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-
 from blog import views
+from blog.views import PostListView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
     path(
         '<int:year>/<int:month>/<int:day>/<str:slug>', 
         views.post_detail, 
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Category, Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# Create your views here.
-def post_list(request):
-    post_list = Post.published.all()
-    categories = Category.objects.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
+# Create class base views
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = "blog/list.html"
 
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
 
-    template =  'blog/list.html'
-    context = {
-        'posts': posts,
-        'categories': categories
-    }
+# # Create your views here.
+# def post_list(request):
+#     post_list = Post.published.all()
+#     categories = Category.objects.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
 
-    return render(request, template, context)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
 
+#     template =  'blog/list.html'
+#     context = {
+#         'posts': posts,
+#         'categories': categories
+#     }
+
+#     return render(request, template, context)
 
 def post_detail(request, year, month, day, slug):
     print(year, month, day, slug)
